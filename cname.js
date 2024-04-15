45576b672bacdcb4d634f8289be695a0392cfb41
Mian Saleem <saleem@tecdiary.com>
2024-04-15 13:16:28 +0800

    Add readme

--- cname.js
+++ cname.js
@@ -2,6 +2,15 @@ import fs from "node:fs";
 
 try {
   fs.writeFileSync("./dist/CNAME", "demo.tec.sh");
+  fs.writeFileSync(
+    "./dist/README.md",
+    `## Tec.sh
+
+  #### A live demo setup to showcase our software, scripts & themes.
+
+  ### Please check [demo.tec.sh](https://demo.tec.sh)
+`
+  );
 } catch (e) {
   console.log(e.toString());
 }
---
+++ dist/README.md
@@ -0,0 +1,5 @@
+## Tec.sh
+
+  #### A live demo setup to showcase our software, scripts & themes.
+
+  ### Please check [demo.tec.sh](https://demo.tec.sh)
